ab2173c53ba79c597e8b4d345f0752e761f5390b
Kemy10 <120021911+Kemy10@users.noreply.github.com>
2023-11-09 11:01:59 -0300

    Add files via upload

---
+++ JavaScript/Exemplos/exemplo.js
@@ -0,0 +1,14 @@
+// document.getElementById server para pegar o valor que esta digitado dentro da variavel que eu declarei
+// document,addEventListener SREVRE PARA DAR UM EVENTO NO MEU CODIGO. UM ALERTA POR EXEMPLO
+// parseInt() -> converte o texto de string para inteiro
+// parseFloat() -> converte o texto de strig para decimais
+// toUpperCase deixa a letra maiuscula
+// toLowerase deixa a letra minuscula
+
+function nomeDaFuncao(nome, idade, sala){
+    alert ('o nome do aluno é' + nome + 'ele tem' + idade + 'anos e estuda na sala' +sala)
+}
+
+
+
+
---
+++ JavaScript/Exercícios de Soma/ex1.js
@@ -0,0 +1,16 @@
+function somar() {
+
+    numero1 = parseInt(document.getElementById('numero1').value)
+    numero2 = parseInt(document.getElementById('numero2').value)
+
+    resultado = numero1 + numero2
+
+    document.getElementById('resultado').innerHTML = resultado
+}
+
+function subtrair() {
+    resultado = 3 - 21
+    document.getElementById('resultado').innerHTML = resultado
+
+
+}
